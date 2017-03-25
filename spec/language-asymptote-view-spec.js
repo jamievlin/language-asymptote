62ac0b9868ae1288259a0c978a706702bbe9a087
Supakorn Rassameemasmuang <mistersupakorn@gmail.com>
2017-03-24 19:05:04 -0600

    Added initial commit

---
+++ spec/language-asymptote-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageAsymptoteView from '../lib/language-asymptote-view';
+
+describe('LanguageAsymptoteView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
